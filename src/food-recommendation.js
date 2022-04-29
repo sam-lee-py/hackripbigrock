2f0baa80dc8e4453cea38750427514ac7fd3b49e
jssnora <js.s@live.com>
2022-04-29 14:55:07 +1000

    food recommendation js file done

--- src/food-recommendation.js
+++ src/food-recommendation.js
@@ -1,15 +1,55 @@
-function getCocktail() {
-    const response = fetch('https://world.openfoodfacts.org/api/v0/product/737628064502.json')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    return response
+getFoodData();
+
+async function getFoodData() {
+    try {
+        const mealResponse = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+        if (mealResponse.ok) {
+            const mealData = await mealResponse.json();
+            displayMeal(mealData);
+        } else {
+            throw new Error (mealData.statusText);
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
 
-function display(input) {
-    let para = document.createElement("p");
-    para.innerHTML = `${input}`;
-    document.querySelector("body").appendChild(para);
+function displayMeal(data) {
+
+    const meal = data.meals[0];
+    const mealDiv = document.getElementById("meal");
+
+    const mealName = meal.strMeal;
+    const heading = document.createElement("h1");
+    heading.innerHTML = mealName;
+    mealDiv.appendChild(heading);
+
+    const mealImg = document.createElement("img");
+    mealImg.src = meal.strMealThumb;
+    mealDiv.appendChild(mealImg);
+    document.body.style.backgroundImage = "url('" + meal.strMealThumb + "')";
+
+    const mealIngredients = document.createElement("ul");
+    document.getElementById("meal").appendChild(mealIngredients);
+
+    const getIngredients = Object.keys(meal)
+    .filter(function (ingredient) {
+    return ingredient.indexOf("strIngredient") == 0;
+    })
+    .reduce(function (ingredients, ingredient) {
+    if (meal[ingredient] != null) {
+    ingredients[ingredient] = meal[ingredient];
+    }
+    return ingredients;
+    }, {});
+
+    for (let key in getIngredients) {
+    let value = getIngredients[key];
+    listItem = document.createElement("li");
+    listItem.innerHTML = value;
+    mealIngredients.appendChild(listItem);
+    }
+
 }
 
-display(getCocktail())
